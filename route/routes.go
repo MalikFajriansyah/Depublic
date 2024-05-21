5833cbc9481d2df88e3902fb5849b9018f04b3b6
MalikFajriansyah <malik04fajriansyah@gmail.com>
2024-05-21 11:40:48 +0700

    updating route for login and register

--- route/routes.go
+++ route/routes.go
@@ -2,11 +2,8 @@ package route
 
 import (
 	"Depublic-App-Service/controller"
-	"Depublic-App-Service/validation"
 
-	echojwt "github.com/labstack/echo-jwt"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 func InitRoutes() {
@@ -18,23 +15,8 @@ func InitRoutes() {
 	event.GET("/events/location/:location", controller.GetEventByLocation)
 	event.GET("/events/search", controller.SearchEventName)
 	event.POST("/addEvent", controller.CreateEvent)
-	// e.Use(echojwt.JWT([]byte("SECRET")))
-
-	// Basic Authentication
-	basicAuth := e.Group("/basicAuth")
-	basicAuth.Use(middleware.BasicAuth(validation.BasicAuthValidator))
-	basicAuth.POST("/login", controller.LoginUser)
-
-	// JWT Authentication
-	jwt := e.Group("/jwt")
-	jwt.Use(echojwt.WithConfig(echojwt.Config{
-		SigningMethod: "HS256",
-		SigningKey:    []byte("SECRET"),
-	}))
-	// role user
-	jwt.GET("/home", controller.DashboardJwt)
 
 	e.POST("/register", controller.RegisterUser)
-	e.POST("/login", validation.LoginUseJwt)
+	e.POST("/login", controller.LoginUser)
 	e.Start(":8080")
 }
