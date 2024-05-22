3b5449ca20130e4c3c0a3916221e2efff10083e1
MalikFajriansyah <malik04fajriansyah@gmail.com>
2024-05-22 09:04:06 +0700

    updating route

--- route/routes.go
+++ route/routes.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"Depublic-App-Service/controller"
+	"Depublic-App-Service/middlewares"
 
 	"github.com/labstack/echo/v4"
 )
@@ -18,5 +19,9 @@ func InitRoutes() {
 
 	e.POST("/register", controller.RegisterUser)
 	e.POST("/login", controller.LoginUser)
+
+	restricted := e.Group("/restricted")
+	restricted.Use(middlewares.Auth)
+	restricted.GET("/home", controller.Home)
 	e.Start(":8080")
 }
